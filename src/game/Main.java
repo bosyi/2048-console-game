517382b75016ad3d681cc26fc4c8328637c35a18
Bogdan <bodya.y@gmail.com>
2016-02-29 20:54:46 +0200

    add possibility to choose side side at the start of game

--- src/game/Main.java
+++ src/game/Main.java
@@ -5,7 +5,15 @@ import java.util.Scanner;
 public class Main {
     private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        Game2048 game2048 = Game2048.getGame();
+        int sideSize;
+        System.out.println("Enter side size of game board. 4 is good, 2 is minimal");
+        try {
+            sideSize = Integer.parseInt(scanner.nextLine());
+        } catch (Exception e) {
+            System.out.println("You entered bad value");
+            return;
+        }
+        Game2048 game2048 = Game2048.getGame(sideSize);
         System.out.println("Score: " + game2048.getScore());
         game2048.printGameView();
 
@@ -18,11 +26,14 @@ public class Main {
     }
 
     private static Game2048.Direction getDirection() {
-        Game2048.Direction direction = null;
+        String msg = "Please enter number 4,8,6, or 2";
+        Game2048.Direction direction;
         String line = scanner.nextLine();
-        int value = getLegalValue(line);
-        if (value == -1) {
-            System.out.println("Please enter number 4,8,6, or 2");
+        int value;
+        if (isValueInteger(line)) {
+            value = Integer.parseInt(line);
+        } else {
+            System.out.println(msg);
             return getDirection();
         }
         switch (value) {
@@ -30,22 +41,20 @@ public class Main {
             case 8: direction = Game2048.Direction.UP; break;
             case 6: direction = Game2048.Direction.RIGHT; break;
             case 2: direction = Game2048.Direction.DOWN;break;
+            default: {
+                System.out.println(msg);
+                return getDirection();
+            }
         }
         return direction;
     }
 
-    private static int getLegalValue(String line) {
-        int value;
+    private static boolean isValueInteger(String line) {
         try {
-            value = Integer.parseInt(line);
+            Integer.parseInt(line);
         } catch (NumberFormatException e) {
-            return -1;
+            return false;
         }
-        if (value == 4 || value == 8
-                || value == 6 || value == 2) {
-            return value;
-        } else {
-            return -1;
-        }
+        return true;
     }
 }
