fe0e7db76db96e0b9598029d43a55b13d0ce127b
Bogdan <bodya.y@gmail.com>
2016-03-01 10:14:56 +0200

    method refactor

--- src/game/Main.java
+++ src/game/Main.java
@@ -5,11 +5,10 @@ import java.util.Scanner;
 public class Main {
     private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        int sideSize;
         System.out.println("Enter side size of game board. 4 is good, 2 is minimal");
-        try {
-            sideSize = Integer.parseInt(scanner.nextLine());
-        } catch (Exception e) {
+        String line = scanner.nextLine();
+        Integer sideSize = getValidInteger(line);
+        if (sideSize == null) {
             System.out.println("You entered bad value");
             return;
         }
@@ -29,10 +28,8 @@ public class Main {
         String msg = "Please enter number 4,8,6, or 2";
         Game2048.Direction direction;
         String line = scanner.nextLine();
-        int value;
-        if (isValueInteger(line)) {
-            value = Integer.parseInt(line);
-        } else {
+        Integer value = getValidInteger(line);
+        if (value == null) {
             System.out.println(msg);
             return getDirection();
         }
@@ -49,12 +46,13 @@ public class Main {
         return direction;
     }
 
-    private static boolean isValueInteger(String line) {
+    private static Integer getValidInteger(String line) {
+        Integer value;
         try {
-            Integer.parseInt(line);
+            value = Integer.parseInt(line);
         } catch (NumberFormatException e) {
-            return false;
+            return null;
         }
-        return true;
+        return value;
     }
 }
